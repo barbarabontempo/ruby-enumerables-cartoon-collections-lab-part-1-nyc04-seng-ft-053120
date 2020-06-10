7cc1a16c9075078f55c7ba8f7158006c99396390
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-10 20:11:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,6 @@ def greet_characters(array)
   array.each do |greeting|
     puts "Hello #{greeting}!"
   end
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
 end
 
 def list_dwarves(array)
